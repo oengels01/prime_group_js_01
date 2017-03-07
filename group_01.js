615d9da592490409045bf18c5943ea13907504f4
Olga Engels <ovengels@yahoo.com>
2017-03-07 09:41:06 -0600

    Update HTML

--- group_01.js
+++ group_01.js
@@ -4,70 +4,61 @@ var boo = ["Boo", "11435", "54000", 3];
 var scout = ["Scout", "6243", "74750", 5];
 var robert = ["Robert", "26835", "66000", 1];
 var mayella = ["Mayella", "89068", "35000", 2];
-
 var employees = [atticus, jem, boo, scout, robert, mayella];
+
+function convertEmployees(initialArray)
+{
+  for(var i = 0; i < initialArray.length; i++)
+  {
+    singleEmployeeArray = initialArray[i];
+    singleEmployeeArray[1] = calculatedEmployee(initialArray[i]);
+    singleEmployeeArray[2] = secondStep(initialArray[i]);
+    singleEmployeeArray[3] = thirdStep(initialArray[i]);
+    console.log(singleEmployeeArray);
+  }
+}
+
+function calculatedEmployee(singleEmployeeArray)
+{
+  var bonusPercent = 0;
+  if(singleEmployeeArray[3] == 5)
+  {
+    bonusPercent += 0.1;
+  }
+  else if (singleEmployeeArray[3] == 4)
+  {
+    bonusPercent += 0.06;
+  }
+  else if (singleEmployeeArray[3] == 3)
+  {
+    bonusPercent += 0.04;
+  }
+
+  if(singleEmployeeArray[1].length > 3)
+  {
+    bonusPercent += 0.05;
+  }
+
+  if(singleEmployeeArray[2] > 65000)
+  {
+    bonusPercent -= 0.01;
+  }
+
+  if(bonusPercent > 0.13)
+  {
+    bonusPercent = 0.13;
+  }
+  else if (bonusPercent < 0)
+  {
+    bonusPercent = 0;
+  }
+  return bonusPercent;
+}
+
+function secondStep(initialArray)
+{return (parseInt((singleEmployeeArray[1]+1)*initialArray[2]));}
+
+function thirdStep(initialArray)
+{return (parseInt((singleEmployeeArray[1]*initialArray[2])));}
+
 convertEmployees(employees);
-
-function convertEmployees(array) {
-  for (var i = 0; i < array.length; i++) {
-    var newEmployeeArray = newEmployee(array[i]);
-    console.log(newEmployeeArray);
-  }
-}
-
-function newEmployee(employeeArray){
-  var resultArray = [];
-  //constructor for new employee array
- resultArray.push(employeeArray[0]);
- resultArray.push(bonusPercent());
-  // this.annualcomp = annualComp;
-  // this.totalbonus = totalBonus;
-  //Figures out employee bonus percentage
-
-  function bonusPercent(){
-    var employeeBonus = 0;
-    if(employeeArray[3]==5){
-      employeeBonus += '.10';
-    } else if (employeeArray[3]==3) {
-      employeeBonus += '.04';
-    } else if (employeeArray[3]==4) {
-      employeeBonus += '.06';
-    } else {
-      employeeBonus += 0;
-    }
-
-      function bonusAdded(){
-        if(employeeArray[1].length==4){
-      employeeBonus += '.05';
-    }
-  }
-    if(employeeArray[2]>65000)
-    {
-      employeeBonus -= '.01';
-    }
-
-    if(employeeBonus > '.13')
-    {
-      employeeBonus = '.13';
-    }
-    else if (employeeBonus < 0) {
-      employeeBonus = 0;
-    }
-    return employeeBonus;
-
-  }
-
-
-//figures out the total bonus
-  var totalBonus = function totalBonus(){
-    var totalBonus = parsInt(employeeBonus*employeeArray[2]);
-    return totalBonus;
-  };
-
-//figures out annual compensation
-  var annualComp = function totalComp(){
-    var total = (totalBonus+employeeArray[2]);
-  };
-
-  return resultArray;
-}
